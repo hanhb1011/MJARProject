5db6d924c7035b831f56888f1f9a1f9840c62339
Munak <oiool@naver.com>
2018-01-19 15:24:14 +0900

    first update

---
+++ MJAR/MJAR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MJAR
+//
+//  Created by WinterSky on 2018. 1. 19..
+//  Copyright © 2018년 WinterSky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
