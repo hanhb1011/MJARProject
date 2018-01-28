2d184babfcd2d2def5e52b299459fb6a3ba6e851
한효병 <hanhyobyung@hanhyobyeongs-Mac.local>
2018-01-28 02:46:36 -0800

    update receiving process

--- HyoByung/HBPart/HBPart/CommentTableViewController.swift
+++ HyoByung/HBPart/HBPart/CommentTableViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class CommentTableViewController: UITableViewController {
-
+    var comments : [Comment] = []
+    var averageRating : Double = 0.0
+    @IBOutlet weak var averageRatingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        getDataFromServer()
+        
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -29,24 +36,66 @@ class CommentTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return comments.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "commentCell", for: indexPath) as!CommentTableViewCell
+        var comment = comments[indexPath.row]
+        
+        cell.titleLabel.text = comment.title!
+        cell.ratingLabel.text = String(comment.rating!)
+        cell.dateLabel.text = comment.date!
+        cell.commentLabel.text = comment.comment!
+        
         return cell
     }
-    */
+    
+    func getDataFromServer(){
+        
+        var ref: DatabaseReference!
+        ref = Database.database().reference().child("comments")
+        
+        //receive data from server
+        ref.observe(.value) { snapshot in
+            let root : NSDictionary = snapshot.value as! NSDictionary
+            let dataset = root.allValues
+            for data in dataset {
+                let dict = data as! NSDictionary
+                let comment = Comment()
+                guard let rating = dict["rating"] as? Int else {
+                    return
+                }
+                comment.commentId = "\(dict["commentId"]!)"
+                comment.rating = Double(rating)
+                comment.comment = "\(dict["comment"]!)"
+                comment.date = "\(dict["date"]!)"
+                comment.title = "\(dict["title"]!)"
+                comment.uid = "\(dict["uid"]!)"
+                comment.password="\(dict["password"]!)"
+                
+                self.averageRating += comment.rating!
+                self.comments.append(comment)
+            }
+            
+            //and update ui
+            if self.comments.count > 0 {
+                self.averageRating /= Double(self.comments.count)
+                self.averageRatingLabel.text = "Average : \(round(self.averageRating*100)/100)"
+                self.tableView.reloadData()
+            }
+        }
+        
+        
+    }
 
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
